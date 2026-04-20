caaf4a278bb261ec39072d5e382b5957b704701e
Lucas Cardoso <lucascardoso.devPython@gmail.com>
2026-04-20 14:27:30 -0300

    simple golem pursuit ai
    implemented simple golem pursuit ai, where it navigates through terain and finds routs to the player avoiding obstacles

--- Assets/Script/Golem.cs
+++ Assets/Script/Golem.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
 
     private SphereCollider visionRange;
+    private Vector3 spawnLocation;
+    private NavMeshAgent agent;
     [SerializeField] private float visionAngle = 80f;
     [SerializeField] private float perceptionDistance = 5;
     [SerializeField] private LayerMask playerLayer;
@@ -11,12 +14,16 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float idleSpeed = 0.5f;
     [SerializeField] private float pursueSpeed = 1;
     [SerializeField] private float turningSpeed = 1;
+    [SerializeField] private float headTurnSpeed = 1.5f;
+    private Vector3 lastTargetPosition = Vector3.zero;
+    private bool searching = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         visionRange = GetComponent<SphereCollider>();
-
+        spawnLocation = transform.position;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void PlayerDetector(Collider target)
@@ -30,32 +37,50 @@ public class Enemy : MonoBehaviour
 
             if (Physics.Raycast(headCenter, targetDirection, distanciaParaAlvo, obstacleLayer))
             {
-                Debug.Log("desvi o jogador!");
                 Debug.DrawRay(headCenter, targetDirection * distanciaParaAlvo, Color.red);
             }
             else if (Physics.Raycast(headCenter, targetDirection, distanciaParaAlvo, playerLayer))
             {
-                Debug.Log("vi o jogador!");
                 Debug.DrawRay(headCenter, targetDirection * distanciaParaAlvo, Color.green);
-                PlayerPursue(target);
+                lastTargetPosition = target.transform.position;
+                searching = true;
             }
         }
     }
 
-    void PlayerPursue(Collider target)
+    void PursuePlayer()
     {
-
-        Vector3 targetPos = target.transform.position;
-        targetPos.y = transform.position.y;
-        Vector3 targetDirection = (targetPos - transform.position).normalized;
-
-        if (targetDirection != Vector3.zero)
+        if (searching)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turningSpeed * Time.deltaTime);
-        }
+            Vector3 adjustedTargetPosition = new Vector3(lastTargetPosition.x, transform.position.y, lastTargetPosition.z);
+            /* Vector3 targetDirection = (adjustedTargetPosition - transform.position).normalized;
 
-        transform.Translate(Vector3.forward * pursueSpeed * Time.deltaTime);
+            if (targetDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turningSpeed * Time.deltaTime);
+            }
+
+            transform.Translate(Vector3.forward * pursueSpeed * Time.deltaTime); */
+
+            agent.SetDestination(adjustedTargetPosition);
+
+            if (Vector3.Distance(adjustedTargetPosition, transform.position) <= 0.5f)
+            {
+                searching = false;
+            }
+        }
+    }
+
+    void Patrol(Vector3 areaCenter)
+    {
+        // ToDo: Implement patrol behavior here: the enemy should move in a radious around the spawn location, changing direction randomly after a certain time or distance.
+        // if it's out of the patrol range, it should start the searchingForPlayer behavior.
+    }
+
+    void Seeking()
+    {
+        // ToDo: Implement searching for player behavior here: the enemy should patrol the last known position of the player and look around for a short time, if it doesn't find the player, it should come back to spawn.
     }
 
     private void OnTriggerStay(Collider other)
@@ -69,6 +94,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PursuePlayer();
     }
 }
