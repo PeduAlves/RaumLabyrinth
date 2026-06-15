99cf6386832402f0f6fdac2ae7d6999fcf5c0cc5
Lucas Cardoso <lucascardoso.devPython@gmail.com>
2026-06-15 15:32:34 -0300

    all basic functions and animations
    done

--- Assets/Script/DataBlocks.cs
+++ Assets/Script/DataBlocks.cs
@@ -13,32 +13,77 @@ public enum EnemyDamagablePart
     Torso
 }
 
-public enum EnemyState
+public static class EnemyDamagablePartExtensions
 {
-    Broke,
+    public static EnemyConditions GetContionOfPartDamage(this EnemyDamagablePart part)
+    {
+        return part switch
+        {
+            EnemyDamagablePart.Head => EnemyConditions.Broken,
+            EnemyDamagablePart.LeftArm => EnemyConditions.BrokeLeftArm,
+            EnemyDamagablePart.RightArm => EnemyConditions.BrokeRightArm,
+            EnemyDamagablePart.RightLeg => EnemyConditions.BrokeRightLeg,
+            EnemyDamagablePart.LeftLeg => EnemyConditions.BrokeLeftLeg,
+            _ => EnemyConditions.Stunned
+        };
+    }
+}
+
+public enum  EnemyConditions
+{
+    Stunned,
+    Broken,
+    BrokeLeftArm,
+    BrokeRightArm,
+    BrokeRightLeg,
+    BrokeLeftLeg,
+}
+
+public static class EnemyConditionsExtensions
+{
+    public static string GetAnimatorConditionName(this EnemyConditions condition)
+    {
+        return condition switch
+        {
+            EnemyConditions.Broken => "Broken",
+            EnemyConditions.Stunned => "Stunned",
+            EnemyConditions.BrokeLeftArm => "Crippled.Arm.L",
+            EnemyConditions.BrokeRightArm => "Crippled.Arm.R",
+            EnemyConditions.BrokeRightLeg => "Crippled.Leg.R",
+            EnemyConditions.BrokeLeftLeg => "Crippled.Leg.L",
+            _ => throw new System.ArgumentOutOfRangeException(nameof(condition), condition, null)
+        };
+    }
+}
+
+public enum EnemyStates
+{
     Looking,
     Walking,
     Attacking,
 }
 
-public static class EnemyStateExtensions
+public static class EnemyStatesExtensions
 {
-    public static string GetAnimationTrigger(this EnemyState state)
+    public static string GetAnimationTrigger(this EnemyStates state)
     {
         return state switch
         {
-            EnemyState.Broke => "Break",
-            EnemyState.Looking => "Look",
-            EnemyState.Walking => "Walk",
-            EnemyState.Attacking => "Attack",
+            EnemyStates.Looking => "Look",
+            EnemyStates.Walking => "Walk",
+            EnemyStates.Attacking => "Attack",
             _ => throw new System.ArgumentOutOfRangeException(nameof(state), state, null)
         };
     }
-}
 
-public enum EnemyMovementState
-{
-    Idle,
-    Moving,
-    Airborne
+    public static int GetStatePriority(this EnemyStates state)
+    {
+        return state switch
+        {
+            EnemyStates.Looking => 0,
+            EnemyStates.Walking => 1,
+            EnemyStates.Attacking => 2,
+            _ => throw new System.ArgumentOutOfRangeException(nameof(state), state, null)
+        };
+    }
 }
--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,211 +4,198 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-
-    // components and references
+    // References
     private SphereCollider visionRange;
-    private Vector3 spawnLocation;
     private NavMeshAgent agent;
+    private Animator animator;
+    private Dictionary<EnemyDamagablePart, EnemyPart> bodyParts = new Dictionary<EnemyDamagablePart, EnemyPart>();
 
     [Header("Perception Settings")]
     [SerializeField] private float visionAngle = 80f;
     [SerializeField] private float perceptionDistance = 5f;
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private float maxSearchTime = 30f;
 
     [Header("Movement Settings")]
     [SerializeField] private float patrolRadius = 7f;
-    [Range(0f, 1f)] private float minPatrolRadiusPercentage = 0.5f;
-    private float minPatrolRadius;
-    [SerializeField] private float maxSearchTime = 30f;
-    [SerializeField] private float maxStunTime = 2f;
+    [Range(0f, 1f)][SerializeField] private float minPatrolRadiusPercentage = 0.5f;
     [SerializeField] private LayerMask groundLayer;
+    private float minPatrolRadius;
+    private Vector3 spawnLocation;
 
     [Header("Attack Settings")]
-    [SerializeField] private SphereCollider rightFistCollider;
-    [SerializeField] private SphereCollider leftFistCollider;
+    [SerializeField] private Collider rightFistCollider;
+    [SerializeField] private Collider leftFistCollider;
     [SerializeField] private float attackRange = 3f;
     [SerializeField] private GameObject shokwavePrefab;
 
     [Header("State Variables")]
-    [SerializeField] private bool isDisabled = false;
-    [SerializeField] private bool isStunned = false;
+    [SerializeField] private EnemyStates currentState;
     [SerializeField] private bool isSearching = false;
-    [SerializeField] private EnemyState[] states = new EnemyState[2];   //ToDo: remove serializefield
-    [SerializeField] private EnemyMovementState movementState;          //ToDo: remove serializefield
+    [SerializeField] private bool isHeadBroken = false;
+    [SerializeField] private bool isMoving = false;
+    [SerializeField] private bool isAirborne = true;
+
     private bool isPlayerInSight = false;
     private Vector3 lastTargetPosition;
     private float searchTimer = 0f;
-    private float stunTimer = 0f;
-    private Dictionary<EnemyDamagablePart, EnemyPart> bodyParts = new Dictionary<EnemyDamagablePart, EnemyPart>();
-    private Animator animator;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        minPatrolRadius = patrolRadius * minPatrolRadiusPercentage;
-        ChangeStates(EnemyState.Broke);
-        movementState = EnemyMovementState.Airborne;
-        visionRange = GetComponent<SphereCollider>();
-        spawnLocation = transform.position;
-        lastTargetPosition = spawnLocation;
         agent = GetComponent<NavMeshAgent>();
-        
-        EnemyPart[] parts = GetComponentsInChildren<EnemyPart>();
-        foreach (EnemyPart part in parts)
+        visionRange = GetComponent<SphereCollider>();
+
+        minPatrolRadius = patrolRadius * minPatrolRadiusPercentage;
+        spawnLocation = lastTargetPosition = transform.position;
+
+        rightFistCollider.enabled = leftFistCollider.enabled = false;
+
+        foreach (var part in GetComponentsInChildren<EnemyPart>())
         {
             bodyParts[part.partType] = part;
         }
     }
 
-    EnemyState getCurrentState()
-    {
-        return states[0];
-    }
-
-    EnemyState GetLastState()
-    {
-        return states[1];
-    }
-
-    void ChangeStates(EnemyState newState)
-    {
-        // This method can be expanded to include any necessary logic when changing states, such as resetting timers or triggering animations.
-        if (getCurrentState() == newState) return;
-        states[1] = states[0];
-        states[0] = newState;
-        animator.SetTrigger(newState.GetAnimationTrigger());
-    }
-
-    void ChangeMovementState(EnemyMovementState newState)
-    {
-        // This method can be expanded to include any necessary logic when changing movement states, such as adjusting speed or enabling/disabling certain behaviors.
-        movementState = newState;
-    }
-
-    public void Stun()
-    {
-        isStunned = true;
-        stunTimer = maxStunTime;
-        agent.isStopped = true;
-        ChangeStates(EnemyState.Broke);
-    }
-
-    void Unstun()
-    {
-        isStunned = false;
-        agent.isStopped = false;
-        ChangeStates(GetLastState());
-    }
-
-    public void DisableAi()
-    {
-        isDisabled = true;
-        agent.enabled = false;
-        ChangeStates(EnemyState.Broke);
-    }
-
-    public void EnableAi()
-    {
-        isDisabled = false;
-        agent.enabled = true;
-        if (isStunned)
-        {
-            Unstun();
-        }
-        else
-        {
-            ChangeStates(GetLastState());
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (movementState == EnemyMovementState.Airborne)
-        {
-            if (((1 << collision.gameObject.layer) & groundLayer) != 0)
-            {
-
-                agent.enabled = true;
-
-                Destroy(GetComponent<Rigidbody>());
-                Destroy(GetComponent<BoxCollider>());
-                ChangeMovementState(EnemyMovementState.Idle);
-                ChangeStates(EnemyState.Looking);
-            }
-        }
-    }
-
-    Vector3 ChooseRandomPoint(Vector3 startingPoint)
-    {
-        // 1. Gera uma direção aleatória em um círculo (eixo X e Z)
-        Vector2 randomDir = Random.insideUnitCircle.normalized;
-
-        // 2. Define uma distância aleatória entre o raio mínimo e o máximo
-        float randomDistance = Random.Range(minPatrolRadius, patrolRadius);
-
-        // 3. Calcula a posição final
-        Vector3 point = new Vector3(
-            randomDir.x * randomDistance,
-            0,
-            randomDir.y * randomDistance
-        );
-
-        return startingPoint + point;
-    }
-
-    void GoToLocation(Vector3 location)
-    {
-        ChangeMovementState(EnemyMovementState.Moving);
-        ChangeStates(EnemyState.Walking);
-
-        Vector3 adjustedTargetPosition = new Vector3(location.x, transform.position.y, location.z);
-        if (agent.isStopped) agent.isStopped = false;
-        agent.SetDestination(adjustedTargetPosition);
-
-    }
-
-    void Patrol(Vector3 areaCenter)
-    {
-        GoToLocation(ChooseRandomPoint(areaCenter));
-    }
-
-    // Update is called once per frame
-    // Working here primarely as a state machine controller
     void Update()
     {
-        if (isDisabled) return;
-
-        if (isStunned)
-        {
-            stunTimer -= Time.deltaTime;
-            if (stunTimer <= 0f)
-            {
-                Unstun();
-            } else return;
-        }
+        if (IsBroken()) return;
 
         if (isSearching)
         {
             searchTimer -= Time.deltaTime;
-
-            if (searchTimer <= 0f)
-            {
-                isSearching = false;
-            }
+            if (searchTimer <= 0f) isSearching = false;
         }
 
-        if (movementState == EnemyMovementState.Moving && !agent.pathPending && agent.remainingDistance <= 0.1f)
+        if (isMoving && !agent.pathPending && agent.remainingDistance <= 0.1f)
         {
-            ChangeMovementState(EnemyMovementState.Idle);
-            ChangeStates(EnemyState.Looking);
+            ResetAi();
         }
     }
 
-    public void FinishLooking()
+    // --- STATE MACHINE & CONDITIONS ---
+    void ChangeStates(EnemyStates newState, bool force = false)
     {
-        if (isSearching) Patrol(lastTargetPosition);
-        else Patrol(spawnLocation);
+        if (currentState == newState || IsBroken()) return;
+        if (!force && currentState.GetStatePriority() > newState.GetStatePriority()) return;
+
+        currentState = newState;
+        animator.SetTrigger(newState.GetAnimationTrigger());
+    }
+
+    void SetCondition(EnemyConditions condition, bool value) => animator.SetBool(condition.GetAnimatorConditionName(), value);
+    bool GetCondition(EnemyConditions condition) => animator.GetBool(condition.GetAnimatorConditionName());
+    bool IsBroken() => GetCondition(EnemyConditions.Broken);
+
+    private bool CheckIfPartBroken()
+    {
+        bool anyArm = GetCondition(EnemyConditions.BrokeLeftArm) || GetCondition(EnemyConditions.BrokeRightArm);
+        bool anyLeg = GetCondition(EnemyConditions.BrokeLeftLeg) || GetCondition(EnemyConditions.BrokeRightLeg);
+        bool bothLegs = GetCondition(EnemyConditions.BrokeLeftLeg) && GetCondition(EnemyConditions.BrokeRightLeg);
+
+        return bothLegs || (anyArm && anyLeg) || isHeadBroken;
+    }
+
+    // --- AI CONTROLLERS ---
+    public void DisableAi()
+    {
+        SetCondition(EnemyConditions.Broken, true);
+        agent.enabled = false;
+        isMoving = false;
+    }
+
+    public void EnableAi() => SetCondition(EnemyConditions.Broken, false);
+
+    public void FinishEnableAi()
+    {
+        agent.enabled = true;
+        Patrol(spawnLocation);
+    }
+        
+
+    public void ResetAi()
+    {
+        isMoving = false;
+        ChangeStates(EnemyStates.Looking, true);
+    }
+
+    // --- MOVEMENT ---
+    void GoToLocation(Vector3 location)
+    {
+        isMoving = true;
+        ChangeStates(EnemyStates.Walking);
+        agent.isStopped = false;
+        agent.SetDestination(new Vector3(location.x, transform.position.y, location.z));
+    }
+
+    void Patrol(Vector3 areaCenter)
+    {
+        Vector2 randomDir = Random.insideUnitCircle.normalized;
+        float randomDist = Random.Range(minPatrolRadius, patrolRadius);
+        Vector3 point = new Vector3(randomDir.x * randomDist, 0, randomDir.y * randomDist);
+
+        GoToLocation(areaCenter + point);
+    }
+
+    public void FinishLooking() => Patrol(isSearching ? lastTargetPosition : spawnLocation);
+
+    // --- PERCEPTION & COLLISIONS ---
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isAirborne && ((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            isAirborne = false;
+            if (!IsBroken()) agent.enabled = true;
+
+            Destroy(GetComponent<Rigidbody>());
+            Destroy(GetComponent<BoxCollider>());
+            ResetAi();
+            Patrol(spawnLocation);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & playerLayer) != 0) PlayerDetector(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (IsBroken() || ((1 << other.gameObject.layer) & playerLayer) == 0) return;
+
+        if (isPlayerInSight) PlayerSpotted(null);
+    }
+
+    void PlayerDetector(Collider target)
+    {
+        if (IsBroken() ||
+            !agent.enabled ||
+            currentState == EnemyStates.Attacking) return;
+
+        Vector3 targetPos = target.transform.position;
+        Vector3 headCenter = transform.TransformPoint(visionRange.center);
+        float targetDistance = Vector3.Distance(headCenter, targetPos);
+
+        if (targetDistance <= attackRange)
+        {
+            ExecuteAttack(targetPos);
+            return;
+        }
+
+        Vector3 targetDirection = (targetPos - headCenter).normalized;
+
+        if (Vector3.Angle(transform.forward, targetDirection) < visionAngle / 2f || targetDistance < perceptionDistance)
+        {
+            if (Physics.Raycast(headCenter, targetDirection, targetDistance, obstacleLayer))
+            {
+                PlayerSpotted(null);
+            }
+            else if (Physics.Raycast(headCenter, targetDirection, targetDistance, playerLayer))
+            {
+                PlayerSpotted(targetPos);
+            }
+        }
     }
 
     private void PlayerSpotted(Vector3? targetPosition)
@@ -216,13 +203,13 @@ public class Enemy : MonoBehaviour
         if (targetPosition.HasValue)
         {
             isPlayerInSight = true;
-            if (Vector3.Distance(lastTargetPosition, targetPosition.Value) > 1f || getCurrentState() != EnemyState.Walking)
+            if (Vector3.Distance(lastTargetPosition, targetPosition.Value) > 1f || currentState != EnemyStates.Walking)
             {
                 lastTargetPosition = targetPosition.Value;
                 GoToLocation(lastTargetPosition);
             }
         }
-        else
+        else if (isPlayerInSight)
         {
             isPlayerInSight = false;
             isSearching = true;
@@ -230,135 +217,59 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (((1 << other.gameObject.layer) & playerLayer) != 0)
-        {
-            if (isPlayerInSight)
-            {
-                PlayerSpotted(null);
-            }
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (((1 << other.gameObject.layer) & playerLayer) != 0)
-        {
-            PlayerDetector(other);
-        }
-    }
-
-    void PlayerDetector(Collider target)
-    {
-        if (isDisabled || isStunned || getCurrentState() == EnemyState.Attacking) return;
-
-        Vector3 headCenter = transform.TransformPoint(visionRange.center);
-        Vector3 targetDirection = (target.transform.position - headCenter).normalized;
-        float targetDistance = Vector3.Distance(headCenter, target.transform.position);
-
-        // 1. Faz as checagens de visão PRIMEIRO, independente de onde o alvo está
-        if ((Vector3.Angle(transform.forward, targetDirection) < visionAngle / 2) ||
-            (Vector3.Distance(transform.position, target.transform.position) < perceptionDistance))
-        {
-            if (Physics.Raycast(headCenter, targetDirection, targetDistance, obstacleLayer))
-            {
-                Debug.DrawRay(headCenter, targetDirection * targetDistance, Color.red);
-                PlayerSpotted(null);
-            }
-            else if (Physics.Raycast(headCenter, targetDirection, targetDistance, playerLayer))
-            {
-                Debug.DrawRay(headCenter, targetDirection * targetDistance, Color.green);
-
-                PlayerSpotted(target.transform.position);
-            }
-        }
-
-        if (Vector3.Distance(transform.position, target.transform.position) <= attackRange && getCurrentState() != EnemyState.Attacking)
-        {
-            ExecuteAttack(target.transform.position);
-        }
-    }
-
+    // --- COMBAT & DAMAGE ---
     internal void TakeDamage(EnemyDamagablePart part)
     {
-        if (part == EnemyDamagablePart.Head)
-        {
-            DisableAi();
-        }
-        else if (part == EnemyDamagablePart.LeftArm || part == EnemyDamagablePart.RightArm)
-        {
-            Stun();
-        }
-        else if (part == EnemyDamagablePart.LeftLeg || part == EnemyDamagablePart.RightLeg)
+        if (part == EnemyDamagablePart.Head) isHeadBroken = true;
+        else if (part != EnemyDamagablePart.Torso)
         {
             agent.acceleration *= 0.5f;
             agent.speed *= 0.5f;
-            // cripped = true;
-            Stun();
-        }
-        else if (part == EnemyDamagablePart.Torso)
-        {
-            throw new System.NotImplementedException("Mas O torço é implacável!");
+            SetCondition(part.GetContionOfPartDamage(), true);
         }
 
+        if (CheckIfPartBroken()) DisableAi();
     }
 
     internal void Heal(EnemyDamagablePart part)
     {
-        if (part == EnemyDamagablePart.Head)
-        {
-            EnableAi();
-        }
-        else if (part == EnemyDamagablePart.LeftLeg || part == EnemyDamagablePart.RightLeg)
+        if (part == EnemyDamagablePart.Head) isHeadBroken = false;
+        else if (part != EnemyDamagablePart.Torso)
         {
             agent.acceleration *= 2f;
             agent.speed *= 2f;
-            // cripped = false;
+            SetCondition(part.GetContionOfPartDamage(), false);
         }
-        else if (part == EnemyDamagablePart.Torso)
-        {
-            throw new System.NotImplementedException("Standard healing");
-        }
+
+        if (!CheckIfPartBroken()) EnableAi();
     }
 
     void ExecuteAttack(Vector3 playerPos)
     {
-        if (bodyParts[EnemyDamagablePart.RightArm].isDisabled && bodyParts[EnemyDamagablePart.LeftArm].isDisabled)
-        {
-            // Both arms are disabled, cannot attack
-            return;
-        }
+        if (bodyParts[EnemyDamagablePart.RightArm].isDisabled && bodyParts[EnemyDamagablePart.LeftArm].isDisabled) return;
 
-        ChangeMovementState(EnemyMovementState.Idle);
+        isMoving = false;
         agent.isStopped = true;
-        ChangeStates(EnemyState.Attacking);
+        ChangeStates(EnemyStates.Attacking);
 
-        if (!bodyParts[EnemyDamagablePart.RightArm].isDisabled)
-            rightFistCollider.enabled = true;
-            
-        if (!bodyParts[EnemyDamagablePart.LeftArm].isDisabled)
-            leftFistCollider.enabled = true;
-        
+        rightFistCollider.enabled = !bodyParts[EnemyDamagablePart.RightArm].isDisabled;
+        leftFistCollider.enabled = !bodyParts[EnemyDamagablePart.LeftArm].isDisabled;
     }
 
     void SpawnShokwave()
-    {
+    {// não sei pq, mas as mãos estão invertidas,
+     // então o shokwave da mão direita sai da mão esquerda e vice versa,
+     // isso é algo que pode ser corrigido futuramente, mas por enquanto é mais fácil deixar assim do que arrumar a animação
         if (!bodyParts[EnemyDamagablePart.RightArm].isDisabled)
-        {
-            Instantiate(shokwavePrefab, rightFistCollider.transform.position, shokwavePrefab.transform.rotation);
-        }
+            Instantiate(shokwavePrefab, leftFistCollider.transform.position, shokwavePrefab.transform.rotation);
 
         if (!bodyParts[EnemyDamagablePart.LeftArm].isDisabled)
-        {
-            Instantiate(shokwavePrefab, leftFistCollider.transform.position, shokwavePrefab.transform.rotation);
-        }
+            Instantiate(shokwavePrefab, rightFistCollider.transform.position, shokwavePrefab.transform.rotation);
     }
 
-    void EndAttack() {
-        ChangeStates(EnemyState.Looking);
-        rightFistCollider.enabled = false;
-        leftFistCollider.enabled = false;
+    void EndAttack()
+    {
+        ResetAi();
+        rightFistCollider.enabled = leftFistCollider.enabled = false;
     }
-
 }
